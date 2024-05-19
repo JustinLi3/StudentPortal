28a23df76493ab067c2822f346f8f6ee0f8a5684
Justin_Li <Triamethyest@gmail.com>
2024-05-19 14:33:34 -0400

    Add project files.

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Web.Data
+{
+  public class ApplicationDbContext: DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+      //Setting up database connection by using 'options'
+    {
+        
+    }
+  }
+}
---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+  public class Student
+  { 
+    public Guid Id { get; set; } //Guid represents a unique identifier for the student entity (primary columns)
+    public string Name { get; set; }  //Want to capture only Name/Email/PhoneNum/Subscribed as Id is automatically set
+    public string Email { get; set; } //Capture through crud operations
+    public string PhoneNum { get; set; } 
+    public bool Subscribed { get; set; } 
+  }
+}
