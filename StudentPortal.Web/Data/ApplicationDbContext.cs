ebb228040ba628857d5099441ec3ad237123a424
Justin_Li <Triamethyest@gmail.com>
2024-05-20 17:40:50 -0400

    Notes

--- StudentPortal.Web/Data/ApplicationDbContext.cs
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -3,12 +3,13 @@ using StudentPortal.Web.Models.Entities;
 
 namespace StudentPortal.Web.Data
 {
-  public class ApplicationDbContext: DbContext
+  public class ApplicationDbContext: DbContext //To use DbContext, we must create a class that inherits DbContext
   {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) //In order to allow DbContext to do any useful work, we must create an instance
       //Setting up database connection by using 'options'
     {
-    } 
+    }  
+    //DbContext class includes a DbSet<TEntity> property for each entity in the model
     public DbSet<Student> Students {  get; set; } //Referencing a student collection/table by which we can get and set
   }
 }
